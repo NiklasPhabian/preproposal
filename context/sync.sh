6db4d1dc284db1a4e6fb5414f0351a78014c1710
Niklas <griessbaum@gmail.com>
2019-10-09 12:56:52 -0700

    some updates

---
+++ context/sync.sh
@@ -0,0 +1,11 @@
+cp ../../../latex/acronyms.tex acronyms.tex
+cp ../../../latex/preamble.tex preamble.tex
+cp ../../../library/snow.bib snow.bib
+
+
+cp ../../../library/phd.bib phd.bib
+cp ../../../library/scidb.bib scidb.bib
+cp ../../../library/HTM.bib HTM.bib
+cp ../../../library/data_citation.bib data_citation.bib
+cp ../../../library/CS274.bib CS274.bib
+cp ../../../library/CS270.bib CS270.bib
